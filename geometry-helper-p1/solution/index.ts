ebe866750315d163d54aa44c328743fe2eff3395
Renmark Soriano <renmarksoriano@gmail.com>
2025-08-14 22:21:13 -0700

    Removed unneeded files

--- geometry-helper-p1/solution/index.ts
+++
@@ -1,4 +0,0 @@
-import { RoundShape } from "./RoundShape";
-import { SquareShape } from "./SquareShape";
-
-export { RoundShape, SquareShape }
--- geometry-helper-p2/solution/index.ts
+++
@@ -1,8 +0,0 @@
-import { RoundShape } from "./RoundShape";
-import { SquareShape } from "./SquareShape";
-import "./2D/types"
-import "./2D/calculations"
-
-export { RoundShape, SquareShape }
-export * from "./2D/types";
-export * from "./2D/calculations"
